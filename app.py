f82b0eab02f7c6d6fe04c88d1998a38d65d7b3f9
KOMPALALOKESH <83068533+KOMPALALOKESH@users.noreply.github.com>
2024-07-10 17:28:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,5 +107,5 @@ def send_query():
     prompt = prompt + "\nresponse: " + response
     return jsonify({"response": response})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=False)
